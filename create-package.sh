162deef0ac31151ecfd35544a0912d9479d13d29
Jeroen Trompetter <j.trompetter@iknl.nl>
2025-01-02 18:25:10 +0100

    scipt voor het genereren van de package voor gebruik in de pipeline

--- create-package.sh
+++ create-package.sh
@@ -4,42 +4,43 @@ TRUE=1
 FALSE=0
 
 sushi_build_output_path=$PWD/fsh-generated/resources
-echo "pad: "$sushi_build_output_path
+echo "Working path: $sushi_build_output_path"
 
 PrepareOutputDirectories ()
 {
-  ERROR=$FALSE
+  local ERROR=$FALSE
+  echo "Recreating package output location"
 
   if [[ -d "${sushi_build_output_path}/package" ]]
   then
-    removed=`rm -f -v "\"${sushi_build_output_path}/package\"" 2>&1` || ERROR=$TRUE
+    removed=`rm -f -v -r "${sushi_build_output_path}/package" 2>&1` || ERROR=$TRUE
     if [[ ${ERROR} -eq ${TRUE} ]]; then
       echo "ERROR: Cannot remove files in output package directory"
       echo -e "---------------------------\n${removed}\n------------------------------"
       exit 1
     fi
-    removed=`rmdir "${sushi_build_output_path}/package" 2>&1`|| ERROR=$TRUE
-    if [[ ${ERROR} -eq ${TRUE} ]]; then
-      echo "ERROR: Could not remove package directory"
-      echo -e "---------------------------\n${removed}\n--------------------------------"
-      exit 1
-    fi
-    echo "Package directory removed"
   fi
 
   created=`mkdir "${sushi_build_output_path}/package" 2>&1`|| ERROR=$TRUE
   if [[ ${ERROR} -eq ${TRUE} ]]; then
-    echo "ERROR: Package directory could not be recreated"
+    echo "ERROR: Package directory '${sushi_build_output_path}/package' could not be recreated"
     echo -e "--------------------------- \n${created}\n---------------------------------"
     exit 1
   fi
-  echo "Package directory was recreated"
 
+  created=`mkdir "${sushi_build_output_path}/package/examples" 2>&1`|| ERROR=$TRUE
+  if [[ ${ERROR} -eq ${TRUE} ]]; then
+    echo "ERROR: Directory '${sushi_build_output_path}/package/examples' could not be recreated"
+    echo -e "--------------------------- \n${created}\n---------------------------------"
+    exit 1
+  fi
 }
 
-CreateTarFile ()
+CopyProfileFiles ()
 {
+  echo "Copying profile files"
 
+  local ERROR=$FALSE
   FILES=`ls "${sushi_build_output_path}" | grep \StructureDefinition`
 
   for name in ${FILES[@]}
@@ -50,16 +51,57 @@ CreateTarFile ()
     echo "destination:" $destination
     succes=`cp "${source}" "${destination}" 2>&1` || ERROR=$TRUE
     if [[ ${ERROR} -eq ${TRUE} ]]; then
-      echo "ERROR: Unable to copy one or more files to target archive directory"
+      echo "ERROR: Unable to copy profile files to target archive directory, exiting"
       echo -e "--------------------------- \n${succes}\n---------------------------------"
       exit 1
     fi
   done
+}
+
+CopyExampleFiles ()
+{
+  echo "Copying example files"
+
+  local ERROR=$FALSE
+  FILES=`ls "${sushi_build_output_path}" | grep -v '\StructureDefinition\|package\|Parameters\|Implementation'`
+
+  for name in ${FILES[@]}
+  do
+    source="${sushi_build_output_path}/${name}"
+    destination="${sushi_build_output_path}/package/examples/${name}"
+    echo "source:" $source
+    echo "destination:" $destination
+    succes=`cp "${source}" "${destination}" 2>&1` || ERROR=$TRUE
+    if [[ ${ERROR} -eq ${TRUE} ]]; then
+      echo "ERROR: Unable to copy example files to target archive directory, exiting"
+      echo -e "--------------------------- \n${succes}\n---------------------------------"
+      exit 1
+    fi
+  done
+}
+
+CreateTarFile ()
+{
+  local ERROR=$FALSE
+
   package_with_version="${PACKAGE_NAME}-v${VERSION_NUMBER}"
-  tar_filename="${sushi_build_output_path}/${package_with_version}.tgz"
+  tar_filename="${package_with_version}.tgz"
   echo "Compressing profile files to package ${package_with_version}.tgz"
-  result=`tar -cvzf "${tar_filename}" "${sushi_build_output_path}/package" 2>&1` || ERROR=$TRUE
-  [[ ${ERROR} -eq ${TRUE} ]] && "Unable to compress files, check error output" && exit 1
+  cd "${sushi_build_output_path}"
+
+  if [[ -f ${tar_filename} ]]; then
+    echo "Found existing package from previous run, removing it now"
+    result=`rm -f ${tar_filename} 2>&1` || ERROR=$TRUE
+    [[ $ERROR -eq $TRUE ]] && echo "Unable to remove existing package file, exiting" && exit 1
+  fi
+
+  result=`tar -cvzf "${tar_filename}" "package" 2>&1` || ERROR=$TRUE
+  if [[ ${ERROR} -eq ${TRUE} ]]
+  then
+    echo "ERROR: Unable to compress files."
+    echo -e "--------------------------- \n${result}\n---------------------------------"
+    exit 1
+  fi
   echo "files were compressed at ${sushi_build_output_path} to '${package_with_version}.tgz'"
 }
 
@@ -85,5 +127,8 @@ done
 [[ ! $(ls -A ${sushi_build_output_path}) ]] && echo "Output directory is empty" && exit 1
 
 PrepareOutputDirectories
+CopyProfileFiles
+CopyExampleFiles
 CreateTarFile
+
 echo "Done."
