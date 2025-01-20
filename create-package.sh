4ab3e348defc0eddfe84abc536d6e612be648c42
Jeroen Trompetter <j.trompetter@iknl.nl>
2025-01-20 10:27:27 +0100

    Package genereren via het door de IG builder aangemaakt package

--- create-package.sh
+++ create-package.sh
@@ -3,37 +3,52 @@
 TRUE=1
 FALSE=0
 
-sushi_build_output_path=$PWD/fsh-generated/resources
-echo "Working path: $sushi_build_output_path"
+ig_generated_package_name="package.r4.tgz"
+home_directory=$PWD
+ig_builder_output_path="${home_directory}/output"
+package_path="${home_directory}/package"
+working_path="${package_path}/working"
 
-PrepareOutputDirectories ()
+echo "ig builder output path: ${ig_builder_output_path}"
+echo "Working directory: ${working_path}"
+echo "Package output directory: ${output_path}"
+
+PrepareDirectories ()
 {
   local ERROR=$FALSE
   echo "Recreating package output location"
 
-  if [[ -d "${sushi_build_output_path}/package" ]]
+  if [[ -d "${package_path}" ]]
   then
-    removed=`rm -f -v -r "${sushi_build_output_path}/package" 2>&1` || ERROR=$TRUE
+    removed=`rm -f -v -r "${package_path}" 2>&1` || ERROR=$TRUE
     if [[ ${ERROR} -eq ${TRUE} ]]; then
-      echo "ERROR: Cannot remove files in output package directory"
+      echo "ERROR: Cannot remove files in working directory"
       echo -e "---------------------------\n${removed}\n------------------------------"
       exit 1
     fi
   fi
 
-  created=`mkdir "${sushi_build_output_path}/package" 2>&1`|| ERROR=$TRUE
+  created=`mkdir -p "${working_path}" 2>&1`|| ERROR=$TRUE
   if [[ ${ERROR} -eq ${TRUE} ]]; then
-    echo "ERROR: Package directory '${sushi_build_output_path}/package' could not be recreated"
+    echo "ERROR: Working directory '${working_path}' could not be recreated"
     echo -e "--------------------------- \n${created}\n---------------------------------"
     exit 1
   fi
 
-  created=`mkdir "${sushi_build_output_path}/package/examples" 2>&1`|| ERROR=$TRUE
+  created=`mkdir -p "${package_output_path}" 2>&1`|| ERROR=$TRUE
   if [[ ${ERROR} -eq ${TRUE} ]]; then
-    echo "ERROR: Directory '${sushi_build_output_path}/package/examples' could not be recreated"
+    echo "ERROR: Package output direcory '${package_output_path}' could not be recreated"
     echo -e "--------------------------- \n${created}\n---------------------------------"
     exit 1
   fi
+
+  created=`mkdir -p "${example_output_path}" 2>&1`|| ERROR=$TRUE
+  if [[ ${ERROR} -eq ${TRUE} ]]; then
+    echo "ERROR: Working directory '${example_output_path}' could not be recreated"
+    echo -e "--------------------------- \n${created}\n---------------------------------"
+    exit 1
+  fi
+
 }
 
 CopyProfileFiles ()
@@ -41,12 +56,13 @@ CopyProfileFiles ()
   echo "Copying profile files"
 
   local ERROR=$FALSE
-  FILES=`ls "${sushi_build_output_path}" | grep \StructureDefinition`
+  local package_export_path="${working_path}/package"
+  local FILES=`ls "${package_export_path}" | grep --exclude-dir={xml,other} --include=*.json '\StructureDefinition\|package'`
 
   for name in ${FILES[@]}
   do
-    source="${sushi_build_output_path}/${name}"
-    destination="${sushi_build_output_path}/package/${name}"
+    source="${package_export_path}/${name}"
+    destination="${package_output_path}/${name}"
     echo "source:" $source
     echo "destination:" $destination
     succes=`cp "${source}" "${destination}" 2>&1` || ERROR=$TRUE
@@ -58,17 +74,17 @@ CopyProfileFiles ()
   done
 }
 
-CopyExampleFiles ()
+CopyExampleFiles()
 {
   echo "Copying example files"
-
   local ERROR=$FALSE
-  FILES=`ls "${sushi_build_output_path}" | grep -v '\StructureDefinition\|package\|Parameters\|Implementation'`
+  local package_export_path="${working_path}/package"
+  local FILES=`ls "${package_export_path}" | grep --include=*.json -v '\StructureDefinition\|package\|Parameters\|Implementation\|xml\|other'`
 
   for name in ${FILES[@]}
   do
-    source="${sushi_build_output_path}/${name}"
-    destination="${sushi_build_output_path}/package/examples/${name}"
+    source="${package_export_path}/${name}"
+    destination="${example_output_path}/${name}"
     echo "source:" $source
     echo "destination:" $destination
     succes=`cp "${source}" "${destination}" 2>&1` || ERROR=$TRUE
@@ -80,20 +96,30 @@ CopyExampleFiles ()
   done
 }
 
-CreateTarFile ()
+UnpackTarFile()
 {
   local ERROR=$FALSE
 
-  package_with_version="${PACKAGE_NAME}-v${VERSION_NUMBER}"
-  tar_filename="${package_with_version}.tgz"
-  echo "Compressing profile files to package ${package_with_version}.tgz"
-  cd "${sushi_build_output_path}"
+  local source_package_file="${ig_builder_output_path}/${ig_generated_package_name}"
+  echo "package file name: ${source_package_file}"
+  [[ ! -f "${source_package_file}" ]] && echo "Package file '${ig_generated_package_name}' not found in IG output folder" && exit 1
 
-  if [[ -f ${tar_filename} ]]; then
-    echo "Found existing package from previous run, removing it now"
-    result=`rm -f ${tar_filename} 2>&1` || ERROR=$TRUE
-    [[ $ERROR -eq $TRUE ]] && echo "Unable to remove existing package file, exiting" && exit 1
+  local result=`tar -xvzf "${source_package_file}" -C "${working_path}" 2>&1` || ERROR=$TRUE
+  if [[ $ERROR -eq $TRUE ]]; then
+    echo "Unable to extract package generated by IG builder."
+    exit 1
   fi
+}
+
+CreateTarFile()
+{
+  local ERROR=$FALSE
+
+  local tar_filename="${PACKAGE_NAME}.tgz"
+  echo "Compressing profile files to package ${tar_filename}"
+
+  echo "Package directory: ${output_path}"
+  cd "${output_path}"
 
   result=`tar -cvzf "${tar_filename}" "package" 2>&1` || ERROR=$TRUE
   if [[ ${ERROR} -eq ${TRUE} ]]
@@ -102,7 +128,28 @@ CreateTarFile ()
     echo -e "--------------------------- \n${result}\n---------------------------------"
     exit 1
   fi
-  echo "files were compressed at ${sushi_build_output_path} to '${package_with_version}.tgz'"
+  echo "files were compressed at ${output_path} to '${tar_filename}'"
+}
+
+UpdatePackageFile()
+{
+  echo "Updating package.json"
+  local ERROR=$FALSE
+
+  local package_filename="${package_output_path}/package.json"
+  cat "${package_filename}" | jq 'del(.canonical)' | tee "${package_filename}" >/dev/null
+  cat "${package_filename}" | jq 'del(.title)' | tee "${package_filename}" >/dev/null
+  cat "${package_filename}" | jq 'del(.type)' | tee "${package_filename}" >/dev/null
+  cat "${package_filename}" | jq 'del(.url)' | tee "${package_filename}" >/dev/null
+  cat "${package_filename}" | jq 'del(."tools-version")' | tee "${package_filename}" >/dev/null
+  cat "${package_filename}" | jq 'del(."date")' | tee "${package_filename}" >/dev/null
+  cat "${package_filename}" | jq 'del(.license)' | tee "${package_filename}" >/dev/null
+  cat "${package_filename}" | jq 'del(."notForPublication")' | tee "${package_filename}" >/dev/null
+  cat "${package_filename}" | jq  '.name="iknl.fhir.nl.r4.ncr-ehr"' | tee "${package_filename}" >/dev/null
+  cat "${package_filename}" | jq '. += {jurisdiction : "urn:iso:std:iso:3166#NL"}' | tee "${package_filename}" >/dev/null
+  cat "${package_filename}" | jq  '.description="FHIR R4 profiles for the electronic health record (EHR) submissions to the Netherlands Cancer Registry (NCR)"' | tee "${package_filename}" >/dev/null
+  cat "${package_filename}" | jq 'del(."directories")' | tee "${package_filename}" >/dev/null
+  cat "${package_filename}" | jq 'del(."maintainers")' | tee "${package_filename}" >/dev/null
 }
 
 for arg in "$@";
@@ -114,21 +161,22 @@ do
       [[ "${value}" == "${name}" ]] && echo "No package name provided"
       PACKAGE_NAME="${value}"
       ;;
-    --version-number)
-      [[ "${value}" == "${name}" ]] && echo "no version provided." && exit 1
-      VERSION_NUMBER="${value}"
-      ;;
  esac
 done
 
-[[ "${VERSION_NUMBER}" == "" ]] && echo "no version number was provided, use '--version-number' as an argument to specify the name of the version number" && exit 1
 [[ "${PACKAGE_NAME}" == "" ]] && echo "No package name was provided, use '--package-name' as an argument to specify a package name" && exit 1
-[[ ! -d "${sushi_build_output_path}" ]] && echo "output path ${sushi_build_output_path} does not exist" && exit 1
-[[ ! $(ls -A ${sushi_build_output_path}) ]] && echo "Output directory is empty" && exit 1
+[[ ! -d "${ig_builder_output_path}" ]] && echo "IG output path ${ig_builder_output_path} does not exist" && exit 1
+[[ ! $(ls -A ${ig_builder_output_path}) ]] && echo "IG Output directory is empty" && exit 1
 
-PrepareOutputDirectories
+output_path="${package_path}/output/${PACKAGE_NAME}"
+package_output_path="${output_path}/package"
+example_output_path="${package_output_path}/examples"
+
+PrepareDirectories
+UnpackTarFile
 CopyProfileFiles
 CopyExampleFiles
+UpdatePackageFile
 CreateTarFile
 
 echo "Done."
